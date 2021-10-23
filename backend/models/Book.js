480642f82f210ba64c096ba3ab0b4698b3eda79e
Said Mounaim <saidmounaim00@gmail.com>
2021-10-23 21:59:01 +0100

    Add Book Model

---
+++ backend/models/Book.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const BookSchema = mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true,
+    },
+
+    description: {
+        type: String,
+        required: true,
+    },
+
+    cover: {
+        type: String,
+        required: true,
+    },
+
+    authors: [
+        {
+            fullName: {
+                type: String,
+                required: true
+            }
+        }
+    ]
+
+}, {
+    timestamps: true
+});
+
+
+const Book = mongoose.model("Book", BookSchema);
+
+
+export default Book;
