f37cb241b5c771d4722bda32d87eca34ffce2c3d
Said Mounaim <saidmounaim00@gmail.com>
2021-10-30 21:14:17 +0100

    Create Book

--- backend/controllers/BookController.js
+++ backend/controllers/BookController.js
@@ -1,28 +1,38 @@
 import asyncHandler from "express-async-handler";
-import Book from '../models/Book.js';
+import Book from "../models/Book.js";
 
 // @DESC Get All Books
 // @ROUTE /api/books
 // @METHOD GET
 export const getAllBooks = asyncHandler(async (req, res) => {
-
-    const books = await Book.find({});
-    res.status(201).json({ success: true, count: books.length, data: books });
-
-})
+  const books = await Book.find({});
+  res.status(201).json({ success: true, count: books.length, data: books });
+});
 
 // @DESC Get Single Book
 // @ROUTE /api/books/:id
 // @METHOD GET
 export const getSingleBook = asyncHandler(async (req, res) => {
+  const book = await Book.findById(req.params.id);
 
-    const book = await Book.findById(req.params.id);
+  if (!book) {
+    res.status(404);
+    throw new Error("Book Not Found");
+  }
 
-    if(!book) {
-        res.status(404);
-        throw new Error("Book Not Found");
-    }
+  res.status(201).json({ success: true, data: book });
+});
 
-    res.status(201).json({ success: true, data: book });
+// @DESC Create Book
+// @ROUTE /api/books
+// @METHOD POST
+export const createBook = asyncHandler(async (req, res) => {
+  const book = await Book.create({
+    title: req.body.title,
+    description: req.body.description,
+    cover: req.body.cover,
+    authors: req.body.authors,
+  });
 
-})
+  res.status(201).json({ success: true, data: book });
+});
--- backend/routes/BookRoute.js
+++ backend/routes/BookRoute.js
@@ -1,11 +1,13 @@
-import express from 'express';
-import { getAllBooks, getSingleBook } from '../controllers/BookController.js';
+import express from "express";
+import {
+  getAllBooks,
+  getSingleBook,
+  createBook,
+} from "../controllers/BookController.js";
 
 const router = express.Router();
 
-
-router.route("/").get(getAllBooks);
-router.route("/:id").get(getSingleBook)
-
+router.route("/").get(getAllBooks).post(createBook);
+router.route("/:id").get(getSingleBook);
 
 export default router;
