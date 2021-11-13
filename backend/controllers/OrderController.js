ce489d9d58521182e1235f0eaaf321159a2fc9af
Said Mounaim <saidmounaim00@gmail.com>
2021-11-13 14:52:34 +0100

    Add Order

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -1,16 +1,47 @@
 import asyncHandler from "express-async-handler";
 import Order from "../models/Order.js";
+import Book from "../models/Book.js";
 
 // @DESC Get All Orders
 // @ROUTE /api/orders
 // @METHOD GET
 export const getOrders = asyncHandler(async (req, res) => {
-  if (!req.user.isAdmin) {
-    res.status(401);
-    throw new Error("Not Authorize to access this route");
+  if (req.user) {
+    if (!req.user.isAdmin) {
+      res.status(401);
+      throw new Error("Not Authorize to access this route");
+    }
   }
 
   const orders = await Order.find({});
 
-  res.status(201).json({ success: true, count: orders.length, data: book });
+  res.status(201).json({ success: true, count: orders.length, data: orders });
+});
+
+// @DESC Add Order
+// @ROUTE /api/orders/book/:bookID
+// @METHOD POST
+export const addOrder = asyncHandler(async (req, res) => {
+  const book = await Book.findById(req.params.bookID);
+
+  if (!book) {
+    res.status(401);
+    throw new Error("Book ID not found");
+  }
+
+  const checkOrder = await Order.findOne({ book: book._id });
+
+  if (checkOrder) {
+    res.status(401);
+    throw new Error("This book already ordered");
+  }
+
+  const order = await Order.create({
+    book: book._id,
+    user: req.user._id,
+    adresse: req.body.adresse,
+    amount: req.body.amount,
+  });
+
+  res.status(201).json({ success: true, order });
 });
--- backend/routes/OrderRoute.js
+++ backend/routes/OrderRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getOrders } from "../controllers/OrderController.js";
+import { getOrders, addOrder } from "../controllers/OrderController.js";
 import { ProtectMiddleware } from "../middlewares/ProtectMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(ProtectMiddleware, getOrders);
+router.route("/book/:bookID").post(ProtectMiddleware, addOrder);
 
 export default router;
